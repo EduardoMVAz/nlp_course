4f747fb5a2f8dfd282471d6cb313076f06e98ea5
EduardoMVAz <emendesvaz@gmail.com>
2025-02-25 16:01:52 -0300

    primeiras aulas

--- nlp_course/crawler.py
+++ nlp_course/crawler.py
@@ -1,9 +1,14 @@
 import queue
+import re
 from typing import List
 
 import requests
 from bs4 import BeautifulSoup
 
+EMAIL_REGEX = r"[A-Za-z]\w*([-\_.]?\w+)*@\w+([-\._]\w+)*"
+HASTHAGE_REGEX = r"#."
+AT_REGEX = r"@\w*"
+TELEPHONE_REGEX = ""
 
 def get_data(url: str) -> List[str]:
     try:
@@ -11,6 +16,10 @@ def get_data(url: str) -> List[str]:
     except requests.exceptions.Timeout:
         return []
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    emails = re.findall(EMAIL_REGEX, response.text)
+    hashtags = re.findall(HASTHAGE_REGEX, response.text)
+    ats = re.findall(AT_REGEX, response.text)
 
     title = soup.title.string if soup.title else 'No title found'
 
@@ -21,7 +30,7 @@ def get_data(url: str) -> List[str]:
             continue
         if target_url.startswith('http'):
             links.append(target_url)
-    return links, title
+    return links, title, emails, hashtags, ats
 
 
 def crawl(
@@ -42,8 +51,8 @@ def crawl(
         visited.add(url)
 
         print(f'Now visiting: {url}')
-        links, title = get_data(url)
-        saved_info.append((title, url))
+        links, title, emails, hashtags, ats = get_data(url)
+        saved_info.append((title, url, emails, hashtags, ats))
         for link in links:
             if link not in visited:
                 q.put(link)
